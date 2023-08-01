ac4a3c75552f78e09584f8420b743bd6fdb9800e
Bruno Alpachino <brunolalpachino@gmail.com>
2023-08-01 11:52:27 +0300

    setup the browser router for the application

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,22 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Cart from "./components/Cart";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
+      <Header />
       <BrowserRouter>
-        <div className="App"></div>
+        <div className="App">
+          <Route path="/" component={<Home />} />
+          <Route path="/" component={<Cart />} />
+        </div>
       </BrowserRouter>
       <p>app</p>
     </>
