d73bc6ccb66cdc09110a14b3ebc9fe0ecd57e6e3
Bruno Alpachino <brunolalpachino@gmail.com>
2023-08-06 08:51:10 +0300

    added body data to send to the backend

--- src/hooks/api-hook.js
+++ src/hooks/api-hook.js
@@ -6,13 +6,15 @@ const useApi = () => {
   // logic errors and loading state
   const [loading, setLoading] = useState();
   const [error, setError] = useState();
-
+  const data = { car: "Mercedes Benz", YOM: "2007", make: "W203" };
   //   use effect to run the custom api
   // receives the type of request that it to be passed
   useEffect(() => {
     // api we will make requests to https://react-fundamentals-c4ee7-default-rtdb.firebaseio.com
     const apiRequest = () => {
-      const request = axios.post(`${import.meta.env.VITE_API_}`);
+      const request = axios.post(`${import.meta.env.VITE_API_}`, {
+        body: data,
+      });
     };
   }, []);
 
