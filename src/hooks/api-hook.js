c1e003209b92b7a545270d5bc2f62d5b2a1d9780
Bruno Alpachino <brunolalpachino@gmail.com>
2023-08-06 08:44:39 +0300

    imported the environment variable from the env

--- src/hooks/api-hook.js
+++ src/hooks/api-hook.js
@@ -11,7 +11,9 @@ const useApi = () => {
   // receives the type of request that it to be passed
   useEffect(() => {
     // api we will make requests to https://react-fundamentals-c4ee7-default-rtdb.firebaseio.com
-    const apiRequest = () => {};
+    const apiRequest = () => {
+      const request = axios.post(`${import.meta.env.VITE_API_}`);
+    };
   }, []);
 
   return { loading, error, apiRequest };
