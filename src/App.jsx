d81a9ef4c809485b108e4f97a1ef28d715854564
Bruno Alpachino <brunolalpachino@gmail.com>
2023-08-19 03:47:38 +0300

    created data hand off function in the custom api hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
@@ -7,21 +7,37 @@ import Header from "./components/Header";
 import Home from "./components/Home";
 import Cart from "./components/Cart";
 // import useCounter from "./hooks/use-counter";
-import { useEffect } from "react";
 import useApi from "./hooks/api-hook";
 
 function App() {
+  const [loadedData, setLoadedData] = useState();
   const url = import.meta.env.VITE_API_URL;
-  const httpData = useApi({ url: `${url}` });
-  const { loading, error, apiRequest, getRequest } = httpData;
+  // function to handle the handoff of data
+  const transformData = (taskObj) => {
+    const loadedTask = [];
+    for (const taskKey in taskObj) {
+      loadedTask.push({ id: taskKey, text: taskObj[taskKey].text });
+    }
+    setLoadedData(loadedTask);
+  };
+  const httpData = useApi({ url: `${url}` }, transformData);
+  const { loading, error, apiRequest, getRequest: getData } = httpData;
 
   // const onClickButtonHandler = () => {
   //   apiRequest();
   // };
+  useEffect(() => {
+    // function called when the component loads to het data
+    // getData();
+  }, []);
+  console.log(loadedData);
   return (
     <>
+      {/* {loadedData.map((item) => {
+        return <p>{item.car}</p>;
+      })} */}
       <div>
-        <button onClick={getRequest}>mock request</button>
+        <button onClick={getData}>mock request</button>
       </div>
     </>
   );
